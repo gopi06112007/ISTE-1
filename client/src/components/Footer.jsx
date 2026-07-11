072ec132df2dfea40feab1af1ec8de8022231515
gopi06112007 <gopi06112007@gmail.com>
2026-07-11 18:32:23 +0530

    fix(footer): update contact email to iste_student@gmrit.edu.in

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -77,7 +77,7 @@ const Footer = ({ forceRender = false }) => {
                 <svg className="w-4 h-4 text-iste-blue flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <span>iste@gmrit.edu.in</span>
+                <span>iste_student@gmrit.edu.in</span>
               </li>
             </ul>
           </div>
